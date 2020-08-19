8b188f157da5a2a95d2327eca0f954507e83c82e
computer-mann <hpsn16@gmail.com>
2020-08-19 22:11:11 +0000

    yooo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,14 @@
+import { AppComponent } from './app.component';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
 const routes: Routes = [
-  /* {},
-  {},
-  {} */
+   {path:"login", component: LoginComponent},
+   {path:"register", component: RegisterComponent},
+   {path:"", component: AppComponent}
 ];
 
 @NgModule({
